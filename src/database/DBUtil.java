a9a6ca402d4b1f5d778d50ae5a8db87063e846b4
Gabriel Dewes <gabrieldewes>
2016-06-30 04:44:30 -0300

    Opção populate no .jar

--- src/database/DBUtil.java
+++ src/database/DBUtil.java
@@ -15,6 +15,77 @@ import java.util.logging.Logger;
  */
 public class DBUtil {
     
+    public void createDDL() {
+        try {
+            DBHelper db = new DBHelper();
+            db.rawLineSQL(create_ddl);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public void dropDDL() {
+        try {
+            DBHelper db = new DBHelper();
+            db.rawLineSQL(drop_ddl);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void populate() {
+        try {
+            DBHelper db = new DBHelper();
+            db.rawLineSQL(insert_data);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void clear() {
+        try {
+            DBHelper db = new DBHelper();
+            db.rawLineSQL(clear_data);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void onUpgrade(DBHelper db, int oldVersion, int newVersion) {
+        String vers[];
+        try {
+            for (int i=oldVersion; i<newVersion; i++) {
+                vers = DBUtil.selectScript(i);
+                DBUtil.updateTabelasBanco(db, vers[0], vers[1], vers[2], vers[3]);
+            }
+
+        } catch (Exception e) {
+        }
+    }
+    
+    public static String VER_1[] =  {"pessoa", "generated", "TEXT", "gabriel"};
+    public static String[] selectScript(int ver){
+        switch (ver) {
+        case 1:
+            return VER_1;
+        default:
+            return null;         
+        }
+
+    }
+    
+    public static void updateTabelasBanco(DBHelper db, String table, String column, String typ, String valor) {
+        try {
+            db.rawSQL("ALTER TABLE " + table + " ADD " + column + " " + typ);
+            if (!"".equals(valor)){
+                db = new DBHelper();
+                db.rawSQL("UPDATE "+ table +" SET "+ column +" = '"+ valor +"'");
+            }
+        } catch (Exception e) {
+        }
+    }
+    
     static String[] create_ddl = {
         "CREATE TABLE IF NOT EXISTS turma ( " +
             "id_turma INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
@@ -77,75 +148,62 @@ public class DBUtil {
         "DELETE FROM emprestimo; "
     };
     
-    public void createDDL() {
-        try {
-            DBHelper db = new DBHelper();
-            db.rawLineSQL(create_ddl);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
+    static String[] insert_data= {
+        "INSERT INTO livro (titulo, autor) VALUES ('Alice no pais das maravilhas', ''); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Água para elefantes', 'Sara Cruen'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Sol e Lua', 'Signos'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('O avantajado livro de pensamentos do cassera & planeta', 'Casseta e Planeta'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('As melhores piadas do planeta e do casseta também', 'Casseta e Planeta'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('A Cabana', 'Artista desconhecido'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Cidades de Papel', 'Albert richerd'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Estrelas de Biscoito', 'Artista aluado'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Como criar uma mente', 'Algum engenheiro da google'); ",
+        "INSERT INTO livro (titulo, autor) VALUES ('Forest gump é mato', 'Racionais'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (1, '1', '', 'x', 'y'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (2, '2', '', 'x1', 'y1'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (3, '3', '', 'x2', 'y2'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (4, '4', '', 'x3', 'y3'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (5, '5', '', 'x4', 'y4'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (6, '6', '', 'x4', 'y4'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (7, '7', '', 'x', 'y'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (8, '8', '', 'x1', 'y1'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (9, '9', '', 'x2', 'y2'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (10, '10', '', 'x3', 'y3'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (11, '11', '', 'x4', 'y4'); ",
+        "INSERT INTO exemplar (id_livro, codigo, disponivel, coordenada_x, coordenada_y) VALUES (12, '12', '', 'x4', 'y4'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('A1', 'Primeiro'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('A2', 'Primeiro'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('B1', 'Segundo'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('B2', 'Segundo'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('C1', 'Terceiro'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('C2', 'Terceiro'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('D1', 'Quarto'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('D2', 'Quarto'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('E1', 'Quinto'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('E2', 'Quinto'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('F1', 'Sexto'); ",
+        "INSERT INTO turma (nome, ano) VALUES ('F2', 'Sexto'); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('1', 'Gabriel Dewes', 'Diretor(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('2', 'Gabriela Santos', 'Diretor(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('3', 'Gabriela Fernandes', 'Aluno(a)', 1); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('4', 'Camila Oliveira', 'Aluno(a)', 3); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('5', 'Fernanda Caroline', 'Aluno(a)', 4); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('6', 'Alana Rapariga', 'Aluno(a)', 5); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('7', 'Ramon Lummertz', 'Aluno(a)', 5); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('8', 'Gustavo Nascimento', 'Aluno(a)', 1); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('9', 'Isabele Guilhermina', 'Funcionário(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('10', 'Bruna Meneguel', 'Funcionário(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('11', 'Gabriel Oliveira', 'Diretor(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('12', 'Bruna Gertz', 'Diretor(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('13', 'Thaynara Adriana', 'Aluno(a)', 1); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('14', 'Adriane Mattos', 'Aluno(a)', 3); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('15', 'Andressa Freitas', 'Aluno(a)', 4); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('16', 'Júlio Cesar', 'Aluno(a)', 7); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('17', 'Laura Dewes', 'Aluno(a)', 6); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('18', 'Maria Júlia', 'Aluno(a)', 6); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('19', 'Julia Farias', 'Funcionário(a)', 0); ",
+        "INSERT INTO pessoa (codigo, nome, cargo, id_turma) VALUES ('20', 'Ana Julia', 'Funcionário(a)', 0); ",
+    };
     
-    public void dropDDL() {
-        try {
-            DBHelper db = new DBHelper();
-            db.rawLineSQL(drop_ddl);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void populate() {
-        try {
-            DBHelper db = new DBHelper();
-            db.rawExternalSQL("insert.sql");
-        } catch (SQLException ex) {
-            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void clear() {
-        try {
-            DBHelper db = new DBHelper();
-            db.rawLineSQL(clear_data);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void onUpgrade(DBHelper db, int oldVersion, int newVersion) {
-        String vers[];
-        try {
-            for (int i=oldVersion; i<newVersion; i++) {
-                vers = DBUtil.selectScript(i);
-                DBUtil.updateTabelasBanco(db, vers[0], vers[1], vers[2], vers[3]);
-            }
-
-        } catch (Exception e) {
-        }
-    }
-    
-    public static String VER_1[] =  {"pessoa", "generated", "TEXT", "gabriel"};
-    public static String[] selectScript(int ver){
-        switch (ver) {
-        case 1:
-            return VER_1;
-        default:
-            return null;         
-        }
-
-    }
-    
-    public static void updateTabelasBanco(DBHelper db, String table, String column, String typ, String valor) {
-        try {
-            db.rawSQL("ALTER TABLE " + table + " ADD " + column + " " + typ);
-            if (!"".equals(valor)){
-                db = new DBHelper();
-                db.rawSQL("UPDATE "+ table +" SET "+ column +" = '"+ valor +"'");
-            }
-        } catch (Exception e) {
-        }
-    }
     
 }
